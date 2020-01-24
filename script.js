487529adef0bbfbf7b7055f768e59224caa592c8
molly-yu <55213953+molly-yu@users.noreply.github.com>
2020-01-23 20:08:10 -0500

    Fixed modals

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
-function openModal() {
-    document.getElementById("modal").style.display = "block";
+function openModal(x) {
+    document.getElementById("modal"+x).style.display = "block";
   }
   
-  function closeModal() {
-    document.getElementById("modal").style.display = "none";
+  function closeModal(x) {
+    document.getElementById("modal"+x).style.display = "none";
   }
   
   var slideIndex = 1;
-  showSlides(slideIndex);
   
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+  function plusSlides(n, x) {
+    showSlides((slideIndex += n),x);
   }
   
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
+  function currentSlide(n,x) {
+    showSlides((slideIndex = n),x);
   }
   
-  function showSlides(n) {
+  function showSlides(n, x) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    var slides = document.getElementsByClassName("mySlides-"+x);
     var captionText = document.getElementById("caption");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
   }
